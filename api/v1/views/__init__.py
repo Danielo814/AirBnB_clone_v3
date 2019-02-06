c996dde8fbf3747ddd1ade502999cfe9911fd192
dspham <dsvpham@gmail.com>
2019-02-06 21:46:15 +0000

    added a url prefix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if app_views:
     from api.v1.views.index import *
