08368e72b0e3bf3a45cd735e03c3eacfb49dc847
Daniel Ojeda <211@holbertonschool.com>
2019-02-06 21:11:15 +0000

    app change port to int

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,15 +6,15 @@ from flask import Flask
 from flask import make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 app.url_map.strict_slashes = False
 
-host = getenv('HBNB_API_HOST', default='0.0.0.0')
-port = int(getenv('HBNB_API_PORT', default=5000))
+host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', default='5000'))
 
 
 @app.teardown_appcontext
@@ -27,4 +27,4 @@ def page_not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port)
